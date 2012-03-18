9e6a5b1eadf41d8f82d0e088374e7f141b737641
arbiter <arbiter@nomad-net.info>
2012-03-18 22:04:44 +0000

    Extension dependency set in CmdLinePlugin and CmdPromptTabPlugin, some other small tweaks, versions updated

--- CmdLinePlugin/CmdLineCommandBase.cs
+++ CmdLinePlugin/CmdLineCommandBase.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Collections.Generic;
-using System.Text;
+using Nomad.Commons.Plugin;
 using Nomad.Shared;
 
 namespace Nomad.Plugin.CmdLinePlugin
 {
+  [ExtensionDependency(typeof(CommandLinePlugin))]
   public abstract class CmdLineCommandBase : Component
   {
     protected ITab CurrentTab
