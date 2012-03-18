87b0dec17b462f2e6b3b12d517724f0f19ec7381
arbiter@nomad-net.info <arbiter@nomad-net.info@932b5a6f-0532-0410-98b7-a16f03a4dd07>
2012-03-18 21:47:21 +0000

    Show how to use configure plug-in functionality in CmdLinePlugin

--- trunk/CmdLinePlugin/CommandLinePlugin.cs
+++ trunk/CmdLinePlugin/CommandLinePlugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Nomad.Commons.Plugin;
@@ -7,15 +8,18 @@ using Nomad.Shared;
 namespace Nomad.Plugin
 {
   [Guid("2C3041DD-BA02-408D-A020-09BC7CFBB9B1")]
-  [ExportExtension(typeof(IRunOnce))]
+  [ExportExtension(typeof(IRunOnce), Configure = true)]
   public class CommandLinePlugin : IRunOnce
   {
+    private static int _MaxCommandLength = 4096;
+
     private static void AddCommandLineToTab(ITab tab)
     {
       IFolderView FolderView = tab as IFolderView;
       if (FolderView != null)
       {
         CommandLinePanel CmdLinePanel = new CommandLinePanel();
+        CmdLinePanel.MaxCommandLength = Math.Max(_MaxCommandLength, 0);
         tab.DockControl(DockStyle.Bottom, CmdLinePanel);
         FolderView.CurrentFolderChanged += (sender2, e2) => CmdLinePanel.CurrentFolder = FolderView.CurrentFolder;
       }
@@ -27,6 +31,13 @@ namespace Nomad.Plugin
       foreach (ITab NextTab in TabManager)
         AddCommandLineToTab(NextTab);
       TabManager.Added += (sender, e) => AddCommandLineToTab(e.Tab);
+    }
+
+    [ConfigurationProperty("maxCommandLength", DefaultValue = 4096)]
+    public int MaxCommandLength 
+    {
+      get { return _MaxCommandLength; }
+      set { _MaxCommandLength = value; }
     }
   }
 }
